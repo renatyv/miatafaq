bca41621c0c88709a85586c1e219dd6b5a8bb91b
Renat Yuldashev <renatyv@gmail.com>
2018-04-11 14:53:08 +0300

    /add commnad, minor fixes

--- main.js
+++ main.js
@@ -1,65 +1,98 @@
 #!/usr/bin/env node
 const TeleBot = require('telebot');
-const user_added_commands_filename = 'user_commands.json';
 // miatafaq_bot
-const bot = new TeleBot({
-    token:'398889955:AAFxjVgOxWPhxO7S-xKy_sc6A90mFZpQ6Rk',
-    usePlugins: ['botan'],
-    pluginConfig: {
-        botan: 'ede451c4-aff9-4461-b4d4-964d3663694b'
-    }
-});
+// const bot = new TeleBot({
+//     token:'398889955:AAFxjVgOxWPhxO7S-xKy_sc6A90mFZpQ6Rk',
+//     usePlugins: ['botan'],
+//     pluginConfig: {
+//         botan: 'ede451c4-aff9-4461-b4d4-964d3663694b'
+//     }
+// });
+const bot = new TeleBot('584587074:AAEj3lrer4dsh21uxPBgt9M92eMHrSXLDZY');
+
+const spb_miataclub_chat_id = -28715622;
+const tech_chat_id = -1001145216568;
+const miataclub_id = -1001095126053;
+const test_bots_chat_id = -217181742;
 
 const fs = require('fs');
 
-const filename = 'commands.json';
-
-function loadCommandsFromFile(){
+function loadCommandsFromFile(filename){
     let rawdata = fs.readFileSync(filename);  
     return JSON.parse(rawdata);
 }
 
-// function saveCommandsToFile(commands){
-//     let data = JSON.stringify(commands,(k,v)=>v,1);  
-//     fs.writeFile(filename, data,(err) => {
-//       if (err){
-//         console.log('File save error!.');
-//       }else{
-//         console.log('The file has been updated.');  
-//       }
-//     });
-//     return;
-// }
+function saveCommandsToFile(filename,commands){
+    let data = JSON.stringify(commands,(k,v)=>v,1);
+    fs.writeFile(filename, data,(err) => {
+      if (err){
+        console.log('File save error!.');
+      }else{
+        console.log('The file '+filename+' has been updated.');
+        console.log(commands);  
+      }
+    });
+    return;
+}
 
 const reserved_commands_array=['add','faq','мануал','стабы_рыксы'];
-var user_added_commands = loadCommandsFromFile();
 
-function addCommand(msg,new_command,new_command_result){
-    if (reserved_commands_array.indexOf(new_command) == -1){
-        console.log('command '+new_command+' is not reserved');
-        var chat_id = msg.chat.id;
+const default_commands_filename = 'default_commands.json';
+var default_commands = loadCommandsFromFile(default_commands_filename);
+const user_added_commands_filename = 'user_commands.json';
+var user_added_commands = loadCommandsFromFile(user_added_commands_filename);
+var commands = Object.assign(default_commands,user_added_commands);
+
+
+function parseNewCommand(msg){
+    regexp = /\/add ([0-9a-zA-Zа-яА-Я_ёЁ]+)/;
+    match_result = msg.text.match(regexp);
+    console.log(match_result);
+    if (match_result){
+        new_command = match_result[1].toLowerCase();
+        new_command_result = msg.text.replace(regexp,'');
+        return {command:new_command,result:new_command_result};
+    }else{
+        console.log('match error');
+        return -1; 
+    }
+}
+
+function addCommand(msg){
+    var chat_id = msg.chat.id;
+    if((chat_id === tech_chat_id)|| (chat_id === miataclub_id) || (chat_id === test_bots_chat_id)){
         var user_id = msg.from.id;
         var chat_member_promise = bot.getChatMember(msg.chat.id,msg.from.id);
         chat_member_promise.then(function(result) {
-            if (result.ok){
-                var chat_member = result.result;
-                if (chat_member.status === 'administrator'){
-                    console.log('is administrator');
-                    user_added_commands[new_command]=new_command_result;
-                    saveCommandsToFile(user_added_commands)
-                    msg.reply.text(new_command+' добавлено в faq');
+                if (result.ok){
+                    var chat_member = result.result;
+                    if (chat_member.status === 'administrator'){
+                        console.log(chat_member.user.id+' is administrator');
+                        var parsed_command = parseNewCommand(msg);
+                        if (parsed_command === -1){
+                            msg.reply.text('/add команда результат');
+                        }else{
+                            user_added_commands[parsed_command.command]=parsed_command.result;
+                            commands[parsed_command.command]=parsed_command.result;
+                            saveCommandsToFile(user_added_commands_filename,user_added_commands);
+                            console.log('команда "'+new_command+'" добавлена в faq');
+                            msg.reply.text('команда "'+new_command+'" добавлена в faq');    
+                        }
+                    }else{
+                        msg.reply.text('Только админы пополняют faq');
+                        console.log('not admin id='+chat_member.user.id+' tried to add command');
+                    }    
                 }else{
-                    msg.reply.text('Только админы пополняют faq');
-                }    
-            }else{
-                console.log(result);    
-            }
-        }, function(err) {
-            console.log(err);
-        });
+                    console.log(result);    
+                }
+            }, function(err) {
+                console.log('promise error');
+                console.log(err);
+            });
     }else{
-        return msg.reply.text('команда '+new_command+' зарезервирована');
-    }   
+        console.log('wrong chat '+chat_id);
+        msg.reply.text('Добавлять могут только админы в чате Miataclub.ru или техчате');
+    }
 }
 
 function execUserCommand(msg,props){
@@ -77,31 +110,21 @@ function execUserCommand(msg,props){
         case 'faq':
             console.log('/faq');
             additional_commands = ['/стабы_рыксы','/мануал']
-            var slashed_commands = Object.keys(user_added_commands).map((command)=>'/'+command).concat(additional_commands);
+            var slashed_commands = Object.keys(commands).map((command)=>'/'+command).concat(additional_commands);
             var list_of_commands = slashed_commands.join(', ');
             return msg.reply.text(list_of_commands);
             break;
-        // case 'add':
-        //     var new_command_string = props.match[2].trim();
-        //     console.log('/add '+new_command_string);
-        //     match_result = new_command_string.match(/([0-9a-zA-Zа-яА-Я_ёЁ]+) (.+)$/);
-        //     if (match_result){
-        //         new_command = match_result[1];
-        //         new_command_result = match_result[2];
-        //         return addCommand(msg,new_command,new_command_result);
-        //     }else{
-        //         console.log('match error:');
-        //         console.log(match_result);
-        //         return msg.reply.text('/add команда результат'); 
-        //     }
-        //     break;
+        case 'add':
+            console.log('/add');
+            addCommand(msg);
+            break;
         default:
-            if (Object.prototype.hasOwnProperty.call(user_added_commands,command)){
+            if (Object.prototype.hasOwnProperty.call(commands,command)){
                 console.log('executing /'+command+' from the datastore');
-                return msg.reply.text(user_added_commands[command]);
+                return msg.reply.text(commands[command]);
             }else{
                 console.log('command /'+command+' is unknown');
-                return msg.reply.text('Не знаю "'+command);
+                return msg.reply.text('Не знаю /'+command);
             }
     }
 }
